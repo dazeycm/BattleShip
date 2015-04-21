0ffbedd464c512c605bb5c77b5977d2bd7532a82
Craig Dazey <dazeycm@miamioh.edu>
2015-04-21 17:11:04 -0400

    Skeleton project

---
+++ src/BattleShipClient.java
@@ -0,0 +1,8 @@
+
+public class BattleShipClient {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ src/BattleShipServer.java
@@ -0,0 +1,8 @@
+
+public class BattleShipServer {
+
+	public static void main(String[] args) {
+
+	}
+
+}

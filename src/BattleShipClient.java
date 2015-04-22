4c94291307e22908772c46a629d44793f5dff5aa
Craig Dazey <dazeycm@miamioh.edu>
2015-04-21 20:48:52 -0400

    Clients can now connect to server

--- src/BattleShipClient.java
+++ src/BattleShipClient.java
@@ -1,8 +1,35 @@
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
-public class BattleShipClient {
 
-	public static void main(String[] args) {
-
+public class BattleShipClient{
+	
+	public static void main(String[] args)	{
+		final int PORT = 1993;
+		
+		InetAddress address;
+		String hostName = null;
+		
+		try {
+			address = InetAddress.getLocalHost();
+			hostName = address.getHostName();
+		} catch (UnknownHostException e) {
+			System.out.println("Error getting localhost");
+			e.printStackTrace();
+		}
+		
+		Socket s = null;
+		try {
+			s = new Socket(hostName, PORT);
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		while(true){}
 	}
 
 }
--- src/BattleShipServer.java
+++ src/BattleShipServer.java
@@ -1,8 +1,64 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
-public class BattleShipServer {
 
+public class BattleShipServer	{
+	
+	static final int PORT = 1993;
+	
+	ServerThread playerA;
+	ServerThread playerB;
+	ServerSocket ss = null;
+	
+	public void getClients()	{
+		this.ss = null;
+		
+		try {
+			ss = new ServerSocket(PORT);
+		} catch (IOException e) {
+			System.out.println("Couldn't establish new serverSocket");
+			e.printStackTrace();
+		}
+		
+		Socket client = null;
+		try {
+			client = ss.accept();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		ServerThread player1 = new ServerThread(client);
+		player1.start();
+		
+		try {
+			client = ss.accept();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		ServerThread player2 = new ServerThread(client);
+		player2.start();
+		
+		while(true){}
+		
+	}
+	
 	public static void main(String[] args) {
+		BattleShipServer bss = new BattleShipServer();
+		bss.getClients();
+	}
+}
 
+class ServerThread extends Thread	{
+
+	Socket client;
+	String name;
+	
+	public ServerThread(Socket client) {
+		this.client = client;
 	}
 
+	public void run() {
+		while(true){}
+	}
+	
 }

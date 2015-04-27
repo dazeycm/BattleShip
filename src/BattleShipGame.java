a578d78348be31a7da5035a7bbcfc2ad58bad54a
dazeycm <craigdazey@gmail.com>
2015-04-27 01:32:23 -0400

    Added boards to the game, and prepared the next messages to send

--- src/BattleShipGame.java
+++ src/BattleShipGame.java
@@ -4,6 +4,9 @@ public class BattleShipGame {
 	public ServerThread player1;
 	public ServerThread player2;
 	
+	public Board player1Board;
+	public Board player2Board;
+	
 	public BattleShipGame(ServerThread player1, ServerThread player2) {
 		this.player1 = player1;
 		this.player2 = player2;
@@ -23,7 +26,10 @@ public class BattleShipGame {
 	}
 	
 	public void getShips()	{
+		player1.sendMessage(Protocol.SNAPE);
+		player2.sendMessage(Protocol.SNAPE);
 		
+		//make boards here
 	}
 
 	

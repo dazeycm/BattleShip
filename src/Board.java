63e10a4b142d42d9b3ff23fb1976a264a83d457f
dazeycm <craigdazey@gmail.com>
2015-04-27 01:29:13 -0400

    Added class to represent the board

---
+++ src/Board.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.util.Map.*;
+import javafx.util.*;
+
+public class Board {
+	int[][] board = new int[11][11];
+	HashMap<String, Ship> ships = new HashMap<String, Ship>();
+	
+	public void addShip(String type, ArrayList<Pair> locs)	{
+		Ship ship = new Ship(locs.size(), locs);
+		ships.put(type, ship);
+	}
+	
+	public int numShipsReamining()	{
+		int numShipsLeft = 0;
+		for(Entry<String, Ship> entry : ships.entrySet())	{
+			if(!entry.getValue().isSunk())	{
+				numShipsLeft++;
+			}
+		}
+		return numShipsLeft;
+	}
+	
+	//need method to try and shoot ship
+	
+	
+	
+	
+}

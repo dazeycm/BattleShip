28b85164eb823717e50c2e58a686d11995f1f46c
dazeycm <craigdazey@gmail.com>
2015-04-29 21:34:53 -0400

    Added custom button class for use when display ships

--- src/BSButton.java
+++ src/BSButton.java
@@ -1,10 +1,32 @@
+import java.awt.Color;
+
+import javafx.util.Pair;
+
 import javax.swing.JButton;
 
 
 public class BSButton extends JButton{
+	int butNum;
+	int x;
+	int y;
+	
+	private static final long serialVersionUID = 1090104771604915982L;
 
-	public static void main(String[] args) {
-
+	public BSButton(int butNum, int x, int y)	{
+		super();
+		this.setBackground(Color.BLUE);
+		this.butNum = butNum;
+		this.x = x;
+		this.y = y;
 	}
+	
+	public Pair getXY()	{
+		return new Pair<Integer, Integer>(this.x, this.y);
+	}
+	
+	public String toString()	{
+		return butNum + " " + x + " " + y;
+	}
+	
 
 }

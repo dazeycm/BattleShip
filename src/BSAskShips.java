1fbbc6c9b50984a646eee05cca7f120791ef6c5a
norahusani <husaninm@miamioh.edu>
2015-05-02 13:48:02 -0400

    changing color

--- src/BSAskShips.java
+++ src/BSAskShips.java
@@ -1,8 +1,13 @@
-import java.awt.*;
-import java.awt.event.*;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
-import javax.swing.*;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class BSAskShips extends JPanel {
 	private static final long serialVersionUID = 1929426850051553458L;
@@ -16,53 +21,53 @@ public class BSAskShips extends JPanel {
 	JLabel errorText = new JLabel();
 	@SuppressWarnings("unused")
 	private BattleShipClient client;
-	
-	public BSAskShips(BattleShipClient client)	{
+
+	public BSAskShips(BattleShipClient client) {
 		this.client = client;
 		this.setLayout(null);
+		this.setBackground(new Color(84, 84, 84));
 		clickedButtons = new ArrayList<BSButton>();
-		
+
 		errorText.setBounds(20, 650, 600, 30);
 		this.add(errorText);
-		
+
 		initGridButtons();
 		intitShipButtons();
 		initFrame();
 	}
-	
-	public void initGridButtons()	{
-		//Button information
+
+	public void initGridButtons() {
+		// Button information
 		int id = 0;
 		int x = 0;
 		int y = 0;
 		int width = 45;
 		int height = 45;
-				
-		//Button placement
+
+		// Button placement
 		int count = 0;
 		int gap = 5;
 		int dx = 10;
 		int dy = 10;
 		int starty = 10;
-		
+
 		gridButtonResponder br = new gridButtonResponder();
-		
-		for(int i = 0; i < 100; i++)	{
-			if(i == 0)	{
+
+		for (int i = 0; i < 100; i++) {
+			if (i == 0) {
 				count++;
-			}
-			else if(count < 10)	{
+			} else if (count < 10) {
 				dy += width + gap;
 				count++;
 				y++;
-			} else	{
+			} else {
 				count = 1;
 				dx += width + gap;
 				x++;
 				dy = starty;
 				y = 0;
 			}
-			
+
 			BSButton button = new BSButton(id, x, y);
 			button.setBounds(dx, dy, width, height);
 			button.addActionListener(br);
@@ -70,57 +75,56 @@ public class BSAskShips extends JPanel {
 			id++;
 		}
 	}
-	
-	public void intitShipButtons()	{
+
+	public void intitShipButtons() {
 		shipButtonResponder br = new shipButtonResponder();
-		
+
 		carrier.setFont(new Font("TimesRoman", Font.PLAIN, 12));
 		battleship.setFont(new Font("TimesRoman", Font.PLAIN, 12));
 		submarine.setFont(new Font("TimesRoman", Font.PLAIN, 12));
 		cruiser.setFont(new Font("TimesRoman", Font.PLAIN, 12));
 		patrol.setFont(new Font("TimesRoman", Font.PLAIN, 12));
-		
+
 		carrier.setBounds(185, 610, 145, 40);
 		battleship.setBounds(310, 560, 145, 40);
 		submarine.setBounds(60, 560, 145, 40);
 		cruiser.setBounds(310, 510, 145, 40);
 		patrol.setBounds(60, 510, 145, 40);
-		
+
 		carrier.addActionListener(br);
 		battleship.addActionListener(br);
 		submarine.addActionListener(br);
 		cruiser.addActionListener(br);
 		patrol.addActionListener(br);
-		
+
 		this.add(carrier);
 		this.add(battleship);
 		this.add(submarine);
 		this.add(cruiser);
 		this.add(patrol);
 	}
-	
-	void initFrame()	{
+
+	void initFrame() {
 		frame = new JFrame("Please place your ships!");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.add(this);
-		frame.setBounds(0,0, 525, 700);
+		frame.setBounds(0, 0, 525, 700);
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
 		frame.setResizable(false);
 	}
-	
+
 	public class gridButtonResponder implements ActionListener {
 
 		public void actionPerformed(ActionEvent e) {
 			BSButton button = (BSButton) e.getSource();
-			if(button.isClickable()){
-				if(!button.hasBeenClicked())	{
-					button.setBackground(Color.YELLOW);
+			if (button.isClickable()) {
+				if (!button.hasBeenClicked()) {
+					button.setBackground(Color.GRAY);
 					button.clicked();
 					clickedButtons.add(button);
-				}
-				else	{
-					button.setBackground(Color.BLUE);
+				} else {
+					button.setBackground(new Color(50, 200, 200));
 					button.clicked();
 					clickedButtons.remove(button);
 				}
@@ -129,101 +133,123 @@ public class BSAskShips extends JPanel {
 			}
 		}
 	}
-	
-	public class shipButtonResponder implements ActionListener	{
+
+	public class shipButtonResponder implements ActionListener {
 
 		public void actionPerformed(ActionEvent e) {
-			if(isValidShip(clickedButtons) && clickedButtons.size() > 1)	{
-				if(e.getSource() == carrier)	{
-					if(clickedButtons.size() != 5)
-						errorText.setText("Expected 5 locations to be selected, but found " + clickedButtons.size());
-					else	{
-						for(BSButton button : clickedButtons)	{
+			if (isValidShip(clickedButtons) && clickedButtons.size() > 1) {
+				if (e.getSource() == carrier) {
+					if (clickedButtons.size() != 5)
+						errorText
+								.setText("Expected 5 locations to be selected, but found "
+										+ clickedButtons.size());
+					else {
+						for (BSButton button : clickedButtons) {
 							button.makeUnclickable();
 						}
 						clickedButtons.clear();
-						client.sendMessage(Protocol.HOGWARTS + Protocol.AIRCRAFT_CARRIER + toPointFormat(clickedButtons));
+						client.sendMessage(Protocol.HOGWARTS
+								+ Protocol.AIRCRAFT_CARRIER
+								+ toPointFormat(clickedButtons));
+					}
+				} else if (e.getSource() == battleship) {
+					if (clickedButtons.size() != 4)
+						errorText
+								.setText("Expected 4 locations to be selected, but found "
+										+ clickedButtons.size());
+					else {
+						for (BSButton button : clickedButtons) {
+							button.makeUnclickable();
+						}
+						clickedButtons.clear();
+						client.sendMessage(Protocol.HOGWARTS
+								+ Protocol.BATTLESHIP
+								+ toPointFormat(clickedButtons));
+					}
+				} else if (e.getSource() == submarine) {
+					if (clickedButtons.size() != 3)
+						errorText
+								.setText("Expected 3 locations to be selected, but found "
+										+ clickedButtons.size());
+					else {
+						for (BSButton button : clickedButtons) {
+							button.makeUnclickable();
+						}
+						clickedButtons.clear();
+						client.sendMessage(Protocol.HOGWARTS
+								+ Protocol.SUBMARINE
+								+ toPointFormat(clickedButtons));
+					}
+				} else if (e.getSource() == cruiser) {
+					if (clickedButtons.size() != 3)
+						errorText
+								.setText("Expected 3 locations to be selected, but found "
+										+ clickedButtons.size());
+					else {
+						for (BSButton button : clickedButtons) {
+							button.makeUnclickable();
+						}
+						clickedButtons.clear();
+						client.sendMessage(Protocol.HOGWARTS + Protocol.CRUISER
+								+ toPointFormat(clickedButtons));
+					}
+				} else if (e.getSource() == patrol) {
+					if (clickedButtons.size() != 2)
+						errorText
+								.setText("Expected 2 locations to be selected, but found "
+										+ clickedButtons.size());
+					else {
+						for (BSButton button : clickedButtons) {
+							button.makeUnclickable();
+						}
+						clickedButtons.clear();
+						client.sendMessage(Protocol.HOGWARTS
+								+ Protocol.PATROL_BOAT
+								+ toPointFormat(clickedButtons));
 					}
 				}
-				else if (e.getSource() == battleship)	{
-					if(clickedButtons.size() != 4)
-						errorText.setText("Expected 4 locations to be selected, but found " + clickedButtons.size());
-					else	{
-						for(BSButton button : clickedButtons)	{
-							button.makeUnclickable();
-						}
-						clickedButtons.clear();
-						client.sendMessage(Protocol.HOGWARTS + Protocol.BATTLESHIP + toPointFormat(clickedButtons));					}
-				}
-				else if (e.getSource() == submarine)	{
-					if(clickedButtons.size() != 3)
-						errorText.setText("Expected 3 locations to be selected, but found " + clickedButtons.size());
-					else	{
-						for(BSButton button : clickedButtons)	{
-							button.makeUnclickable();
-						}
-						clickedButtons.clear();
-						client.sendMessage(Protocol.HOGWARTS + Protocol.SUBMARINE + toPointFormat(clickedButtons));					}
-				}
-				else if (e.getSource() == cruiser)	{
-					if(clickedButtons.size() != 3)
-						errorText.setText("Expected 3 locations to be selected, but found " + clickedButtons.size());
-					else	{
-						for(BSButton button : clickedButtons)	{
-							button.makeUnclickable();
-						}
-						clickedButtons.clear();
-						client.sendMessage(Protocol.HOGWARTS + Protocol.CRUISER + toPointFormat(clickedButtons));					}
-				}
-				else if (e.getSource() == patrol)	{
-					if(clickedButtons.size() != 2)
-						errorText.setText("Expected 2 locations to be selected, but found " + clickedButtons.size());
-					else	{
-						for(BSButton button : clickedButtons)	{
-							button.makeUnclickable();
-						}
-						clickedButtons.clear();
-						client.sendMessage(Protocol.HOGWARTS + Protocol.PATROL_BOAT + toPointFormat(clickedButtons));					}
-				}
-			}
-			else	{
+			} else {
 				errorText.setText("Invalid ship placement!");
 			}
 		}
-		
-		public boolean isValidShip(ArrayList<BSButton> buttons){
+
+		public boolean isValidShip(ArrayList<BSButton> buttons) {
 			boolean vertical = true;
 			boolean horizontal = true;
 			boolean allNextTo = true;
-			for(int i = 0; i < clickedButtons.size() - 1 && vertical; i++)	{
-				if(clickedButtons.get(i).x != clickedButtons.get(i + 1).x)
+			for (int i = 0; i < clickedButtons.size() - 1 && vertical; i++) {
+				if (clickedButtons.get(i).x != clickedButtons.get(i + 1).x)
 					vertical = false;
 			}
-			
-			for(int i = 0; i < clickedButtons.size() - 1 && horizontal; i++)	{
-				if(clickedButtons.get(i).y != clickedButtons.get(i + 1).y)
+
+			for (int i = 0; i < clickedButtons.size() - 1 && horizontal; i++) {
+				if (clickedButtons.get(i).y != clickedButtons.get(i + 1).y)
 					horizontal = false;
 			}
-			
-			if(vertical)	{
-				for(int i = 0; i < clickedButtons.size() && allNextTo; i++)	{
-					for(int j = 0; j < clickedButtons.size() && allNextTo; j++)	{
-						allNextTo = Math.abs(clickedButtons.get(i).y - clickedButtons.get(j).y) <= clickedButtons.size() - 1;
+
+			if (vertical) {
+				for (int i = 0; i < clickedButtons.size() && allNextTo; i++) {
+					for (int j = 0; j < clickedButtons.size() && allNextTo; j++) {
+						allNextTo = Math.abs(clickedButtons.get(i).y
+								- clickedButtons.get(j).y) <= clickedButtons
+								.size() - 1;
 					}
 				}
-			} else if (horizontal)	{
-				for(int i = 0; i < clickedButtons.size() && allNextTo; i++)	{
-					for(int j = 0; j < clickedButtons.size() && allNextTo; j++)	{
-						allNextTo = Math.abs(clickedButtons.get(i).x - clickedButtons.get(j).x) <= clickedButtons.size() - 1;
+			} else if (horizontal) {
+				for (int i = 0; i < clickedButtons.size() && allNextTo; i++) {
+					for (int j = 0; j < clickedButtons.size() && allNextTo; j++) {
+						allNextTo = Math.abs(clickedButtons.get(i).x
+								- clickedButtons.get(j).x) <= clickedButtons
+								.size() - 1;
 					}
 				}
 			}
 			return allNextTo && (vertical || horizontal);
 		}
-		
-		public String toPointFormat(ArrayList<BSButton> buttons)	{
+
+		public String toPointFormat(ArrayList<BSButton> buttons) {
 			StringBuilder sb = new StringBuilder();
-			for(BSButton b : buttons)	{
+			for (BSButton b : buttons) {
 				sb.append(b.getXY() + " ");
 			}
 			System.out.println(sb.toString());
--- src/BSButton.java
+++ src/BSButton.java
@@ -1,51 +1,48 @@
 import java.awt.Color;
-import java.awt.Point;
-
-import javafx.util.Pair;
 
 import javax.swing.JButton;
 
-public class BSButton extends JButton{
+public class BSButton extends JButton {
 	int butNum;
 	int x;
 	int y;
 	boolean clicked;
 	boolean clickable;
-	
+
 	private static final long serialVersionUID = 1090104771604915982L;
 
-	public BSButton(int butNum, int x, int y)	{
+	public BSButton(int butNum, int x, int y) {
 		super();
-		this.setBackground(Color.BLUE);
+		this.setBackground(new Color(50, 200, 200));
 		this.butNum = butNum;
 		this.x = x;
 		this.y = y;
 		this.clicked = false;
 		this.clickable = true;
 	}
-	
-	public String getXY()	{
+
+	public String getXY() {
 		return "(" + x + "," + y + ")";
 	}
-	
-	public void clicked()	{
-		if(clickable)
+
+	public void clicked() {
+		if (clickable)
 			this.clicked = !this.clicked;
 	}
-	
-	public boolean hasBeenClicked()	{
+
+	public boolean hasBeenClicked() {
 		return this.clicked;
 	}
-	
-	public void makeUnclickable()	{
+
+	public void makeUnclickable() {
 		this.clickable = false;
 	}
-	
-	public boolean isClickable()	{
+
+	public boolean isClickable() {
 		return this.clickable;
 	}
-	
-	public String toString()	{
+
+	public String toString() {
 		return butNum + " " + x + " " + y;
 	}
 }

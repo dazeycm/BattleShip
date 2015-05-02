19a39fee4b8e5980a2e00180b62b42bb59ff1b06
norahusani <husaninm@miamioh.edu>
2015-05-02 13:36:54 -0400

    added some documentation and color

--- src/BSAskName.java
+++ src/BSAskName.java
@@ -1,8 +1,23 @@
-import java.awt.event.*;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-import javax.swing.*;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
-public class BSAskName extends JPanel	{
+/**
+ * 
+ * @author Craig Dazey and Nora Husani
+ * @course CSE 211
+ * @professor Dr. Kiper
+ * @date 05/07/15 BSAskName is a GUI interface that prompts each player to enter
+ *       their name in order to begin a game of battle ship
+ *
+ */
+public class BSAskName extends JPanel {
 	private static final long serialVersionUID = 2575364361360064192L;
 	private JFrame frame;
 	private JTextField nameEntry;
@@ -11,40 +26,47 @@ public class BSAskName extends JPanel	{
 	public String name = null;
 	@SuppressWarnings("unused")
 	private BattleShipClient client;
-	
-	public BSAskName(final BattleShipClient client)	{
+
+	/**
+	 * 
+	 * @param client
+	 *            a BattleShipClient object that represents one of the players
+	 *            in the game Preconditions: client is not NULL Postconditions:
+	 *            new BSAskName GUI is created Side Effects: this.client is set
+	 *            to client and GUI is set up
+	 */
+	public BSAskName(final BattleShipClient client) {
 		this.client = client;
 		this.setLayout(null);
-		
+		this.setBackground(new Color(50, 200, 200));
 		nameEntry = new JTextField();
 		nameEntry.setBounds(25, 60, 350, 30);
 		this.add(nameEntry);
-		
+
 		enterName = new JButton("Submit");
 		enterName.setBounds(280, 110, 90, 30);
 		enterName.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e)
-            { 
-            	name = nameEntry.getText();
-                frame.setVisible(false);
-                frame.dispose();
-                client.sendMessage(Protocol.SNAPE + name);
-            }
-        });      
+			public void actionPerformed(ActionEvent e) {
+				name = nameEntry.getText();
+				frame.setVisible(false);
+				frame.dispose();
+				client.sendMessage(Protocol.SNAPE + name);
+			}
+		});
 		this.add(enterName);
-		
+
 		enterNameText = new JLabel("Please enter your name:");
 		enterNameText.setBounds(25, 40, 350, 20);
 		this.add(enterNameText);
-		
+
 		initFrame();
 	}
-	
-	void initFrame()	{
+
+	void initFrame() {
 		frame = new JFrame("Welcome to BattleShip!");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.add(this);
-		frame.setBounds(0,0, 400, 200);
+		frame.setBounds(0, 0, 400, 200);
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
 		frame.setResizable(false);
--- src/Protocol.java
+++ src/Protocol.java
@@ -1,12 +1,11 @@
-
 public class Protocol {
-	
+
 	/**
 	 * This is the "what is your name?" message
 	 */
 	public static final String ALBUS = "ALBUS ";
 	/**
-	 * This is the "my name is" message 
+	 * This is the "my name is" message
 	 */
 	public static final String SNAPE = "SNAPE ";
 	/**
@@ -14,7 +13,7 @@ public class Protocol {
 	 */
 	public static final String VOLDY = "VOLDY ";
 	/**
-	 * Holds a type of ship and the coordinates 
+	 * Holds a type of ship and the coordinates
 	 */
 	public static final String HOGWARTS = "HOGWARTS ";
 	/**
@@ -22,27 +21,28 @@ public class Protocol {
 	 */
 	public static final String AIRCRAFT_CARRIER = "AIRCRAFT_CARRIER ";
 	/**
-	 * This is the battleship ship message which has 4 coordinates 
+	 * This is the battleship ship message which has 4 coordinates
 	 */
 	public static final String BATTLESHIP = "BATTLESHIP ";
 	/**
-	 * This is the submarine ship message which has 3 coordinates 
+	 * This is the submarine ship message which has 3 coordinates
 	 */
 	public static final String SUBMARINE = "SUBMARINE ";
 	/**
-	 * This is the cruiser ship message which has 3 coordinates 
+	 * This is the cruiser ship message which has 3 coordinates
 	 */
 	public static final String CRUISER = "CRUISER ";
 	/**
-	 * This is the patrol_boat ship message which has 2 coordinates 
+	 * This is the patrol_boat ship message which has 2 coordinates
 	 */
 	public static final String PATROL_BOAT = "PATROL_BOAT ";
 	/**
-	 * This is the "this is where I want to attack" message 
+	 * This is the "this is where I want to attack" message
 	 */
 	public static final String CURSE = "CURSE ";
 	/**
-	 * This is the response message to an attack which contains 3 types of responses
+	 * This is the response message to an attack which contains 3 types of
+	 * responses
 	 */
 	public static final String AUROR = "AUROR ";
 	/**
@@ -50,47 +50,47 @@ public class Protocol {
 	 */
 	public static final String CRUCIO = "CRUCIO ";
 	/**
-	 * This is the "miss" message 
+	 * This is the "miss" message
 	 */
 	public static final String STUPEFY = "STUPEFY ";
 	/**
-	 * This is the "sunk" message 
+	 * This is the "sunk" message
 	 */
 	public static final String AVADAKEDAVRA = "AVADAKEDAVRA ";
 	/**
-	 * This is the game over message 
+	 * This is the game over message
 	 */
 	public static final String GAME_OVER = "GAME_OVER ";
 	/**
-	 * This is the "yes I want to play again" message 
+	 * This is the "yes I want to play again" message
 	 */
 	public static final String YES = "YES";
 	/**
-	 * This is the "no I don't want to play again" message 
+	 * This is the "no I don't want to play again" message
 	 */
 	public static final String NO = "NO";
 	/**
-	 * This is the stats message 
+	 * This is the stats message
 	 */
 	public static final String HOOCH = "HOOCH";
 	/**
-	 * This is the winner message 
+	 * This is the winner message
 	 */
 	public static final String WINNER = "WINNER";
 	/**
-	 * This is the score message 
+	 * This is the score message
 	 */
 	public static final String SCORE = "SCORE ";
 	/**
-	 * This is the shots for player1 message 
+	 * This is the shots for player1 message
 	 */
 	public static final String SHOTS_P1 = "SHOTS_P1 ";
 	/**
-	 * This is the shots for player2 message 
+	 * This is the shots for player2 message
 	 */
 	public static final String SHOTS_P2 = "SHOTS_P2 ";
 	/**
-	 * This is the accuracy message 
+	 * This is the accuracy message
 	 */
 	public static final String ACCURACY = "ACCURACY";
 }

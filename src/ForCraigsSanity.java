de1e54b1c1ba5153d6c0b3a8ae1d83304a5be9c3
Craig Dazey <dazeycm@miamioh.edu>
2015-05-02 21:17:38 -0400

    Added class so I don't go insane

---
+++ src/ForCraigsSanity.java
@@ -0,0 +1,30 @@
+
+public class ForCraigsSanity {
+	
+	public static void main(String[] args) throws InterruptedException {
+		Object lock = new Object();
+		
+		synchronized  (lock)	{
+			new Thread(new RunServer()).start();
+			lock.wait(250);
+			new Thread(new RunClient()).start();
+			lock.wait(250);
+			new Thread(new RunClient()).start();
+		}
+	}
+	
+	public static class RunServer implements Runnable	{
+		public void run()	{
+			String[] args = new String[0];
+			BattleShipServer.main(args);
+		}
+	}
+	
+	public static class RunClient implements Runnable	{
+		public void run()	{
+			String[] args = new String[0];
+			BattleShipClient.main(args);
+		}
+	}
+
+}

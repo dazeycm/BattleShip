16a0360f9ea3a0e31fcfd2a600d38ccb9615282d
norahusani <husaninm@miamioh.edu>
2015-05-03 13:12:54 -0400

    added prologue documentation forCraigsSanity

--- src/ForCraigsSanity.java
+++ src/ForCraigsSanity.java
@@ -1,10 +1,18 @@
-
+/**
+ * 
+ * @author Craig Dazey and Nora Husani
+ * @course CSE 211
+ * @professor Dr. Kiper
+ * @date 05/07/15
+ * @description ForCraigsSanity is a class for handling the clients, server, and
+ *              threads
+ */
 public class ForCraigsSanity {
-	
+
 	public static void main(String[] args) throws InterruptedException {
 		Object lock = new Object();
-		
-		synchronized  (lock)	{
+
+		synchronized (lock) {
 			new Thread(new RunServer()).start();
 			lock.wait(250);
 			new Thread(new RunClient()).start();
@@ -12,16 +20,16 @@ public class ForCraigsSanity {
 			new Thread(new RunClient()).start();
 		}
 	}
-	
-	public static class RunServer implements Runnable	{
-		public void run()	{
+
+	public static class RunServer implements Runnable {
+		public void run() {
 			String[] args = new String[0];
 			BattleShipServer.main(args);
 		}
 	}
-	
-	public static class RunClient implements Runnable	{
-		public void run()	{
+
+	public static class RunClient implements Runnable {
+		public void run() {
 			String[] args = new String[0];
 			BattleShipClient.main(args);
 		}

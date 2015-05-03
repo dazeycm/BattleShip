1341a3d5452939c3cc24fbf8b8c36dc4d06ecba4
Craig Dazey <dazeycm@miamioh.edu>
2015-05-03 03:16:25 -0400

    delete all remnants of a bad idea

--- src/Board.java
+++ src/Board.java
@@ -1,4 +1,3 @@
-import java.awt.Point;
 import java.util.*;
 import java.util.Map.*;
 
--- src/Ship.java
+++ src/Ship.java
@@ -1,11 +1,8 @@
-import java.awt.Point;
-import java.util.ArrayList;
 import java.util.List;
 
 
 public class Ship {
 	private int health;
-	public int butNum;
 	public List<Integer> locs;
 	
 	public Ship(int health, List<Integer> shipLocs)	{

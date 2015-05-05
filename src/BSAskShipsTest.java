8f2052e3a4f28125881934b2153655ec7b30c50a
norahusani <husaninm@miamioh.edu>
2015-05-05 14:12:06 -0400

    working on more tests for askShips but it ain't working

--- src/BSAskShipsTest.java
+++ src/BSAskShipsTest.java
@@ -1,5 +1,7 @@
 import static org.junit.Assert.assertFalse;
 
+import java.util.ArrayList;
+
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -29,6 +31,13 @@ public class BSAskShipsTest {
 		assertFalse(ask.submarine == null);
 		assertFalse(ask.cruiser == null);
 		assertFalse(ask.patrol == null);
+	}
+
+	@Test
+	public void isValidShipTest() {
+		BSAskShips ask = new BSAskShips();
+		ArrayList<BSButton> buttons;
+		// ask.isValidShip(buttons);
 	}
 
 }

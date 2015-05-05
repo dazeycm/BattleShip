53ca300e09d1788f1ab6054412de48a2a259fb41
norahusani <husaninm@miamioh.edu>
2015-05-05 16:14:43 -0400

    created more ship tests mate

--- src/ShipTest.java
+++ src/ShipTest.java
@@ -1,4 +1,6 @@
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,12 +25,65 @@ public class ShipTest {
 	}
 
 	@Test
+	// shipConstructorTest verifies that the constructor for a ship
+	// correctly instantiates a ship object with the correct health and
+	// locations
 	public void ShipConstructorTest() {
 		List<Integer> intShipLocs = new ArrayList<Integer>();
 		intShipLocs.add(33);
 		intShipLocs.add(34);
 		Ship s = new Ship(2, intShipLocs);
 		assertEquals(2, s.getHealth());
+		assertEquals(2, intShipLocs.size());
+	}
+
+	@Test
+	// shipHitTest verifies that the shipHit method correctly decrements
+	// the health of a ship
+	public void shipHitTest() {
+		List<Integer> intShipLocs = new ArrayList<Integer>();
+		intShipLocs.add(33);
+		intShipLocs.add(34);
+		Ship s = new Ship(2, intShipLocs);
+		s.shipHit();
+		assertEquals(1, s.getHealth());
+	}
+
+	@Test
+	// shipHitTest verifies that the shipHit method sets health to -1
+	// if a ship is hit after it is sunk
+	public void shipHitInvalidTest() {
+		List<Integer> intShipLocs = new ArrayList<Integer>();
+		intShipLocs.add(33);
+		intShipLocs.add(34);
+		Ship s = new Ship(0, intShipLocs);
+		s.shipHit();
+		assertEquals(-1, s.getHealth());
+	}
+
+	@Test
+	// isSunkTrueTest verifies that the isSunk method returns true
+	// when a ship has been sunk
+	public void isSunkTrueTest() {
+		List<Integer> intShipLocs = new ArrayList<Integer>();
+		intShipLocs.add(33);
+		intShipLocs.add(34);
+		Ship s = new Ship(2, intShipLocs);
+		s.shipHit();
+		s.shipHit();
+		assertTrue(s.isSunk());
+	}
+
+	@Test
+	// isSunkFalseTest verifies that the isSunk method returns false
+	// when a ship has not been sunk
+	public void isSunkFalseTest() {
+		List<Integer> intShipLocs = new ArrayList<Integer>();
+		intShipLocs.add(33);
+		intShipLocs.add(34);
+		Ship s = new Ship(2, intShipLocs);
+		s.shipHit();
+		assertFalse(s.isSunk());
 	}
 
 }

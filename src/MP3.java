d8d413f793387a0c0fad80ba46f94172187d383f
norahusani <husaninm@miamioh.edu>
2015-05-06 14:50:09 -0400

    documented MP3

--- src/MP3.java
+++ src/MP3.java
@@ -4,23 +4,42 @@ import java.io.InputStream;
 
 import javazoom.jl.player.Player;
 
+/**
+ * 
+ * @author Craig Dazey and Nora Husani
+ * @course CSE 211
+ * @professor Dr. Kiper
+ * @date 05/07/15
+ * @description MP3 is a class for handling audio for a battleship game
+ */
 public class MP3 {
 	private String filename;
 	private Player player;
 	public BufferedInputStream bis;
 
-	// constructor that takes the name of an MP3 file
+	// default constructor
 	public MP3() {
-
 	}
 
+	/**
+	 * Preconditions: none Postconditions: closes the player Side Effects: calls
+	 * .close()
+	 * 
+	 * @throws IOException
+	 */
 	public void close() throws IOException {
 		if (player != null)
 			player.close();
-
 	}
 
 	// play the MP3 file to the sound card
+	/**
+	 * Preconditions: filename is not null Postconditions: audio file is played
+	 * Side Effects: new BufferedInputStream and Player are created
+	 * 
+	 * @param filename
+	 *            a string representing the audio file to play
+	 */
 	public void play(String filename) {
 		try {
 
